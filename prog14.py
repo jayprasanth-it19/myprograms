d4869b547b1252ede611fa3adab8b77d73988157
jayprasanth-it19 <54205884+jayprasanth-it19@users.noreply.github.com>
2019-11-28 12:04:10 +0530

    fourth commit

---
+++ prog14.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+if(a>b and a>c):
+  print(a,"is greater")
+elif(b>c):
+  print(b,"is greater")
+else:
+  print(c,"is greater")
